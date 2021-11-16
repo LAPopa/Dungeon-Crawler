c6702f0ac8b1ba3bfa2194ffe34c375dddb7acd7
LA Popa <laura_stanescu_1991@yahoo.com>
2021-11-16 11:21:37 +0200

    implement fighting, passive npc
    - player deals and takes damage according to strength, armor
    - passive "actor" - objects that don't deal damage
    - TODO : inventory, door, key, death condition, show ingame stats

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Skeleton.java
@@ -3,12 +3,19 @@ package com.codecool.dungeoncrawl.logic.actors;
 import com.codecool.dungeoncrawl.logic.Cell;
 
 public class Skeleton extends Actor {
+//    private String name;
+//    private int health;
+//    private int strength;
     public Skeleton(Cell cell) {
+
         super(cell);
+        this.setHealth(25);
+        this.setStrength(5);
     }
 
     @Override
     public String getTileName() {
+
         return "skeleton";
     }
 }
